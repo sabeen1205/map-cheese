ff5880e6e617d8c31d0fa308e3ac17bf75b4e454
Sabeen1205 <sabeen1205@gmail.com>
2021-02-18 18:03:49 -0800

    testing

---
+++ main.py
@@ -0,0 +1,24 @@
+print("----------------map & cheese-----------------")
+print("welcome to map&cheese")
+print("visit our website -> map&cheese.com")
+print("our buissness hours are at the bottom of our page")
+print("---------------------------------------------\n")
+
+first_name = input("Enter your first name: ")
+last_name = input("enter your last name: ")
+age = int(input("enter your first name: "))
+print(f"full name:{first_name} {last_name}")
+correct_name = input("is your name entered correctly?: ")
+if correct_name !='yes'
+first_name = input("Enter your first name: ")
+last_name = input("enter your last name: ")
+print(f"updated full name: {first_name} {last_name}")
+
+phone_number = input("enter your phone: ")
+print (f"you entered {phone_number} for your phone number")
+
+age = int(input("enter your age: "))
+print(f"age: {age}")
+if correct_age != "yes":
+age = int(input("enter your age: "))
+print(f"upadted age: {age}")
